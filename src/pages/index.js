e064b5602197521aaf9d1be73a72cdc2400af714
Thiago Murakami <thiago.murakami@gmail.com>
2020-04-01 11:22:47 -0300

    Change layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,8 +9,8 @@ import Layout from "../components/Layout"
 import News from "../components/News"
 import RelevantData from "../components/RelevantData"
 import SEO from "../components/seo"
-import Team from "../components/Team"
 import Status from "../components/Status"
+import Team from "../components/Team"
 
 const IndexPage = () => (
   <>
@@ -23,37 +23,14 @@ const IndexPage = () => (
         href="https://www.charidy.com/vempraguerra"
       >
         <Banner />
-        {/* <button
-          type="button"
-          style={{
-            '-moz-appearance': 'none',
-            '-webkit-appearance': 'none',
-            // background: '#dfa86f',
-            background: 'rgb(36, 35, 30)',
-            border: 0,
-            borderRadius: 4,
-            bottom: 0,
-            // color: '#7C2F2E',
-            color: 'rgb(227, 168, 106)',
-            cursor: 'pointer',
-            fontSize: '2rem',
-            fontWeight: 'bold',
-            outline: 0,
-            padding: '1rem',
-            position: 'absolute',
-            right: 0,
-          }}
-        >
-          Doar agora
-        </button> */}
       </a>
     </div>
     <Layout>
       <Status />
       <About />
+      <News />
       <Faq />
       <RelevantData />
-      <News />
       <Team />
       <DonateButton />
     </Layout>
