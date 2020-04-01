0a0ade10236e7c4896a32de9a66c54801346ef79
Thiago Murakami <thiago.murakami@gmail.com>
2020-04-01 11:22:05 -0300

    Update Team

--- src/components/Team.js
+++ src/components/Team.js
@@ -15,29 +15,37 @@ const Team = () => (
     </p>
 
     <ul>
-      <li>Alexandre Vasserman</li>
+      <li>Albert Mazuz</li>
       <li>Alex Faiwichow</li>
+      <li>Alexandre Vasserman</li>
       <li>Ayumi Tomishige</li>
+      <li>Bruno Montico Costa</li>
+      <li>Caio Fernandes Alfieri</li>
       <li>Dan Novachi</li>
       <li>Eduardo Mitelman</li>
       <li>Fabianna Mainier</li>
       <li>George Queiroz</li>
       <li>Guilherme Souza</li>
-      <li>Gustavo Bezerra</li>
       <li>Gunter Sarfert</li>
+      <li>Gustavo Bezerra</li>
       <li>Heloisa Aun</li>
       <li>Hugo Metzger</li>
-      <li>Marcela Butros</li>
-      <li>Natsumi Tomishige</li>
       <li>James Feeler</li>
+      <li>João Paulo Possos Ferreira</li>
       <li>Júlia Ferraz</li>
       <li>Júnior Santos</li>
+      <li>Lucas Wagner</li>
+      <li>Marcela Butros</li>
       <li>Marco Avanzi</li>
+      <li>Mariana Montag</li>
+      <li>Mark Traiman Coji</li>
+      <li>Natsumi Tomishige</li>
       <li>Patrícia Melhem</li>
       <li>Pedro Alexandria</li>
       <li>Pedro Bacchi</li>
       <li>Ramon Ballverdú</li>
       <li>Renilson Barros</li>
+      <li>Ricardo Martins</li>
       <li>Ricardo Vasserman</li>
       <li>Tatiane Menezes</li>
       <li>Thiago Murakami</li>
