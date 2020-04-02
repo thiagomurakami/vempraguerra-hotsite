85c71f683eb035eca68d07ae1f6a5824cbdd7526
Thiago Murakami <thiago.murakami@gmail.com>
2020-04-02 18:22:22 -0300

    Add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,6 +46,19 @@ module.exports = {
         hostname: "www.vempraguerra.com.br",
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-162614932-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: true,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: false,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
