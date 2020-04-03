0ac63b91c32705a946f69a59cb306f4fb1420767
Thiago Murakami <thiago.murakami@gmail.com>
2020-04-02 22:54:38 -0300

    Use sheets for news and team

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,22 @@
-import React from "react"
+import React from 'react'
 
-import About from "../components/About"
-import Banner from "../components/images/Banner"
+import About from '../components/About'
+import Banner from '../components/images/Banner'
 import DonateButton from '../components/DonateButton'
-import Faq from "../components/Faq"
-import Header from "../components/Header"
-import Layout from "../components/Layout"
-import News from "../components/News"
-import RelevantData from "../components/RelevantData"
-import SEO from "../components/seo"
-import Status from "../components/Status"
-import Team from "../components/Team"
+import Faq from '../components/Faq'
+import Header from '../components/Header'
+import Layout from '../components/Layout'
+import News from '../components/News'
+import RelevantData from '../components/RelevantData'
+import SEO from '../components/seo'
+import Status from '../components/Status'
+import Team from '../components/Team'
 
 const IndexPage = () => (
   <>
     <Header />
     <SEO title="#VemPraGuerra" />
-    <div style={{position: 'relative'}}>
+    <div style={{ position: 'relative' }}>
       <a
         target="_blank"
         rel="noopener noreferrer"
